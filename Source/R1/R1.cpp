8af85eed949b89852594b14c237eb30af613ab24
Supply_depot <hj06869@naver.com>
2026-08-24 20:30:35 +0900

    R1 UE Initial commit

---
+++ Source/R1/R1.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "R1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, R1, "R1" );
