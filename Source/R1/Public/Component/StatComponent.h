1422e5e4d8ef5920ee92698054d0c71e4a79780d
Salmiakki-unreal10 <kjk84908490@gmail.com>
2026-08-26 09:59:23 +0900

    디렉토리 수정

---
+++ Source/R1/Public/Component/StatComponent.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Interface/HealthInterface.h"
+#include "Interface/HungerInterface.h"
+#include "Interface/StaminaInterface.h"
+#include "Interface/StatusEffectInterface.h"
+#include "Interface/TemperatureInterface.h"
+#include "Interface/ThirstInterface.h"
+#include "StatComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class R1_API UStatComponent : public UActorComponent//, public IHealthInterface, public IStaminaInterface, public IHungerInterface, public ITemperatureInterface, public IThirstInterface, public IStatusEffectInterface
+{
+	GENERATED_BODY()
+
+public:
+	UStatComponent();
+
+	//virtual float GetCurrentStamina_Implementation() const override;
+	//virtual float GetMaxStamina_Implementation() const override;
+	//virtual bool ConsumeStamina_Implementation(float InAmount) override;
+	//virtual void RecoverStamina_Implementation(float InAmount) override;
+
+	//virtual float GetCurrentHunger_Implementation() const override;
+	//virtual bool IncreaseHunger_Implementation(float InAmount) override;
+	//virtual void RecoverHunger_Implementation(float InAmount) override;
+
+	//virtual float GetCurrentThirst_Implementation() const override;
+	//virtual bool IncreaseThirst_Implementation(float InAmount) override;
+	//virtual void RecoverThirst_Implementation(float InAmount) override;
+
+	//virtual float GetCurrentTemperature_Implementation() const override;
+	//virtual bool IncreaseTemperature_Implementation(float InAmount) override;
+	//virtual void DecreaseTemperature_Implementation(float InAmount) override;
+
+	//virtual EStatusEffectType GetCurrentStatusEffect_Implementation() override;
+	//virtual void RemoveStatusEffect_Implementation(EStatusEffectType InStatusEffectType) override;
+	//virtual void SetStatusEffect_Implementation(EStatusEffectType InStatusEffectType) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
---
+++ Source/R1/Public/Interface/StatInterface.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "StatInterface.generated.h"
+
+
+UINTERFACE(MinimalAPI)
+class UStatInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class R1_API IStatInterface
+{
+	GENERATED_BODY()
+
+
+};
