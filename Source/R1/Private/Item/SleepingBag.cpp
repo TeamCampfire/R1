272311314f39d82f2836c71773115fd94a8aeba7
Supply_depot <hj06869@naver.com>
2026-09-10 14:40:46 +0900

    feat : 침낭 마무리
    - 여러명이 동시에 잠을 자는 현상 수정
    - 카메라 위치를 애니메이션 머리로 변경

--- Source/R1/Private/Item/SleepingBag.cpp
+++ Source/R1/Private/Item/SleepingBag.cpp
@@ -1,12 +1,20 @@
 ﻿#include "Item/SleepingBag.h"
 #include "Character/ActionCharacter.h"
 #include "Character/ActionPlayerController.h"
+#include "Net/UnrealNetwork.h"
 
 // Sets default values
 ASleepingBag::ASleepingBag()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+}
+
+void ASleepingBag::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ASleepingBag, OccupantCharacter);
 }
 
 FTransform ASleepingBag::GetRespawnTransform_Implementation() const
@@ -17,13 +25,16 @@ FTransform ASleepingBag::GetRespawnTransform_Implementation() const
 void ASleepingBag::Interact_Implementation(APawn* Interactor)
 {
 	if (!HasAuthority() || !Interactor) return;
+	if (OccupantCharacter != nullptr) return;
 
 	AActionCharacter* Player = Cast<AActionCharacter>(Interactor);
 	AActionPlayerController* PC = Cast<AActionPlayerController>(Interactor->GetController());
 	if (Player && PC)
 	{
+		// 지금 침낭을 소유한(잠을 자는 캐릭터)
+		OccupantCharacter = Player;
 		PC->SetRespawnPoint(this);
-		Player->StartSleeping(GetTransform());
+		Player->StartSleeping(this);
 	}
 
 }
@@ -32,6 +43,17 @@ TSoftObjectPtr<UTexture2D> ASleepingBag::GetInteractionIcon_Implementation() con
 {
 	if (!InteractionIcon) return nullptr;
 	return InteractionIcon ;
+}
+
+// 침낭 소유권 해제
+void ASleepingBag::ClearOccupant(AActionCharacter* Character)
+{
+	if (!HasAuthority()) return;
+	// 자고 있던 본인이 일어난 경우에만 침낭을 비움
+	if (OccupantCharacter == Character)
+	{
+		OccupantCharacter = nullptr;
+	}
 }
 
 // Called when the game starts or when spawned
--- Source/R1/Public/Item/SleepingBag.h
+++ Source/R1/Public/Item/SleepingBag.h
@@ -8,6 +8,7 @@
 #include "Item/PlaceableItemBase.h"
 #include "Interface/InteractableInterface.h"
 #include "Interface/RespawnPointInterface.h"
+#include "Character/ActionCharacter.h"
 #include "SleepingBag.generated.h"
 
 UCLASS()
@@ -19,14 +20,18 @@ public:
 	// Sets default values for this actor's properties
 	ASleepingBag();
 
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 	virtual FTransform GetRespawnTransform_Implementation() const override;
 
 	//~ Begin IInteractableInterface
 	virtual FText GetInteractionDisplayName_Implementation() const override { return FText::FromString(TEXT("잠자기 (리스폰 등록)")); }
-	virtual bool CanInteract_Implementation(APawn* Interactor) const override { return true; }
+	//누군가 잠자고 있으면 상호작용 불가
+	virtual bool CanInteract_Implementation(APawn* Interactor) const override { return OccupantCharacter == nullptr; }
 	virtual void Interact_Implementation(APawn* Interactor) override;
 	virtual TSoftObjectPtr<UTexture2D> GetInteractionIcon_Implementation() const override;
 	//~ End IInteractableInterface
+
+	void ClearOccupant(AActionCharacter* Character);
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -37,4 +42,9 @@ protected:
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SleepingBag")
 	TSoftObjectPtr<UTexture2D> InteractionIcon;
+
+	// ★ 현재 이 침낭에서 자고 있는 플레이어 (멀티플레이 동기화)
+	UPROPERTY(Replicated)
+	TObjectPtr<AActionCharacter> OccupantCharacter;
+
 };
