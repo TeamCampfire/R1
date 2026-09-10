93f1bfbab2bb160e8b213707339534b2664b917c
Supply_depot <hj06869@naver.com>
2026-09-10 10:14:03 +0900

    feat : Hemp (대마풀 추가)
    - 대마풀용 외부 에셋 추가
    - 대마풀 배치용 BP 추가
    - 대마풀 획득 로직 추가

---
+++ Source/R1/Private/Item/HarvestMisc/Hemp.cpp
@@ -0,0 +1,69 @@
+﻿
+
+
+#include "Item/HarvestMisc/Hemp.h"
+#include "Component/InventoryComponent.h"
+#include "Character/ActionCharacter.h"
+
+// Sets default values
+AHemp::AHemp()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	SetRootComponent(MeshComponent);
+	// 카메라 라인트레이스(ECC_Visibility)에 감지되도록 설정
+	MeshComponent->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
+
+FText AHemp::GetInteractionDisplayName_Implementation() const
+{
+	return DisplayName;
+}
+
+bool AHemp::CanInteract_Implementation(APawn* Interactor) const
+{
+	return YieldItemData != nullptr; 
+}
+
+void AHemp::Interact_Implementation(APawn* Interactor)
+{
+	if (!HasAuthority() || bHarvested || !Interactor || !YieldItemData) return;
+	// 여기까지 들어왔으면 채집완료 표시
+	bHarvested = true;
+
+	if (AActionCharacter* Character = Cast<AActionCharacter>(Interactor))
+	{
+		if (UInventoryComponent* Inven = Character->GetInventoryComponent())
+		{
+			// 아이템 추가
+			int32 Remainder = 0;
+			Inven->AddItem(YieldItemData, YieldCount, Remainder);
+			Inven->NotifyItemAcquired(YieldItemData, YieldCount - Remainder);
+		}
+	}
+
+	Destroy();
+}
+
+TSoftObjectPtr<UTexture2D> AHemp::GetInteractionIcon_Implementation() const
+{
+	return InteractionIcon; 
+}
+
+// Called when the game starts or when spawned
+void AHemp::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHemp::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/R1/Public/Item/HarvestMisc/Hemp.h
@@ -0,0 +1,53 @@
+﻿
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interface/InteractableInterface.h"
+#include "Data/Item/ItemDataBase.h"
+#include "Hemp.generated.h"
+
+UCLASS()
+class R1_API AHemp : public AActor, public IInteractableInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHemp();
+
+	//~ Begin IInteractableInterface
+	virtual FText GetInteractionDisplayName_Implementation() const override;
+	virtual bool CanInteract_Implementation(APawn* Interactor) const override;
+	virtual void Interact_Implementation(APawn* Interactor) override;
+	virtual TSoftObjectPtr<UTexture2D> GetInteractionIcon_Implementation() const override;
+	//~ End IInteractableInterface
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> MeshComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Harvest")
+	FText DisplayName = FText::FromString(TEXT("대마풀"));
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Harvest")
+	TObjectPtr<UItemDataBase> YieldItemData; // DA_Item_Misc_Cloth 지정
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Harvest")
+	int32 YieldCount = 10; // 획득할 천 개수
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Harvest")
+	TSoftObjectPtr<UTexture2D> InteractionIcon;
+
+
+private:
+	bool bHarvested = false;
+};
